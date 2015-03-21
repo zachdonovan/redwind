1c00101662d291a7d2f765262ea3148519879179
Zach Donovan <zach.donovan@gmail.com>
2015-03-20 21:31:57 -0400

    fixup! try/except and print headers instead?

--- redwind/plugins/facebook.py
+++ redwind/plugins/facebook.py
@@ -228,7 +228,10 @@ def handle_new_or_edit(post, preview, img_url, post_type,
         app.logger.debug('Sending post %s', post_args)
         response = requests.post('https://graph.facebook.com/v2.0/me/feed',
                                  data=post_args)
-    response.raise_for_status()
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        app.logger.debug("Got non-200 response from facebook %s", e.headers)
     app.logger.debug("Got response from facebook %s", response)
 
     if 'json' in response.headers['content-type']:
